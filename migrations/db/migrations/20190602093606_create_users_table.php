d329f4bbbbd1491ded9a15a756b9a46e44f11b68
Max Kovalenko <mxss1998@yandex.ru>
2019-06-02 13:24:44 +0300

    added migrations

---
+++ migrations/db/migrations/20190602093606_create_users_table.php
@@ -0,0 +1,17 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateUsersTable extends AbstractMigration
+{
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->addColumn('login', 'string')
+            ->addColumn('password', 'string')
+            ->addColumn('email', 'string', ['null' => true])
+            ->addColumn('register_date', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
+            ->create();
+    }
+}
