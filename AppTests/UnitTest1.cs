39f59d59650985cd414489a428908ab41031a333
Max Kovalenko <mxss1998@yandex.ru>
2019-06-16 15:18:04 +0300

    v0.1 preparation
    Signed-off-by: Max Kovalenko <mxss1998@yandex.ru>

---
+++ AppTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace AppTests {
+    public class Tests {
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
