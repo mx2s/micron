911e54507464b0142a5ccf94da524b4623b5a2ec
Max Kovalenko <mxsshere@gmail.com>
2019-10-19 00:29:33 -0400

    replaced 'Base' with NuGet package (framework core)

--- App/Program.cs
+++ App/Program.cs
@@ -13,11 +13,11 @@ namespace App {
     class Program {
         static void Main(string[] args) {
             var host = new NancyHost(new Bootstrapper(), new Uri("http://localhost:8000"));
-
-            Console.WriteLine("starting server from BaseProject in DEBUG mode");
+            
+            Console.WriteLine("starting server...");
             host.Start();
             Console.WriteLine("server started");
-
+            
             Console.Write("Press enter to stop server...");
             Console.ReadLine();
         }
